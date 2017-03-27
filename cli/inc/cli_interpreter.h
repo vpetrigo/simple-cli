a4ac67d137860ef6f8f98f570f0fc768842fd969
Vladimir Petrigo <taenaru@gmail.com>
2017-03-27 16:35:47 +0300

    Update cli_interpreter.h

--- cli/inc/cli_interpreter.h
+++ cli/inc/cli_interpreter.h
@@ -13,8 +13,19 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/**
+ * \typedef Parser return values representation
+ */
 typedef enum { BAD_REQUEST = 0, COMMAND_PARSED = 1 } CLI_Return_t;
 
+/**
+ * \brief CLI interpreter that process an input string for occurence of command 
+ * and its arguments
+ * \param[in] line String with command and with/without its aruments
+ * \param[in] size Size of **line**
+ * \param[out] cmd Buffer to store command's arguments start/end indexes in a **line**
+ * \param[out] cmd_len How much arguments have been got from **line**
+ */
 uint8_t cli_interpreter(const char *line, size_t size, uint16_t *cmd,
                         size_t *cmd_len);
 
